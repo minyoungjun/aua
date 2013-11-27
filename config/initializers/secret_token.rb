ccd39ef6c9b293e44bfd7ca6a2a534515bfcb273
minyoung <minyoung9353@gmail.com>
2013-11-27 17:50:21 +0900

    f

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aua::Application.config.secret_token = 'b547f5d739a159ca33f656b9c4de160a83f3c5a1f46b19650ab10f9ffc00cdd88b5461a8bea823d51c83103fbb5f7b75b6f644df1926436fc3f1acb39e8aa3e0'
