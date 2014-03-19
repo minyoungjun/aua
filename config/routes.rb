9cb4119054eb9746bc8f69ef86b2d6db5015fb58
root <root@minyoung9353.cafe24.com>
2014-03-19 16:26:17 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Aua::Application.routes.draw do
 
 
-
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,12 +50,41 @@ Aua::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'survey#profile', :id => '1'
-   get 'research' => 'survey#view', :id => '2'
+   root :to => 'survey#index'
+   get 'admin' => 'admin#index', :format => false
+   get 'admin/index' => 'admin#index', :format => false
+   get 'admin/login' => 'admin#login', :format => false
+   post 'admin/login_process' => 'admin#login_process', :format => false
+   get 'admin/logout' => 'admin#logout', :format => false
+   post 'admin/content_up/:id' => 'admin#content_up', :format => false
+   get 'admin/result/:id' => 'admin#result', :format => false
+   get 'admin/destroy_survey/:id' => 'admin#destroy_survey', :format => false
+   get 'admin/new_survey/:id' => 'admin#new_survey', :format => false
+   post 'admin/new_process' => 'admin#new_process', :format => false
+   post 'admin/image_change/:id' => 'admin#image_change', :format => false
+   post 'admin/edit_box/:id' => 'admin#edit_box', :format => false
+   post 'admin/edit_example/:id' => 'admin#edit_example', :format => false
+   post 'admin/delete_example/:id' => 'admin#delete_example', :format => false
+   post 'admin/add_example/:id' => 'admin#add_example, :format => false'
+   post 'admin/add_box/:id' => 'admin#add_box', :format => false
+   post 'admin/add_paragraph/:id' => 'admin#add_paragraph', :format => false
+   get 'admin/edit_survey/:id' => 'admin#edit_survey', :format => false
+   get 'admin/delete_box/:id' => 'admin#delete_box'
+
+
+   get 'survey/index' => 'survey#index', :format => false
+   get 'survey/profile/:id' => 'survey#profile', :format => false
+   post 'survey/profile_process/:id' => 'survey#profile_process', :format => false
+   get 'survey/view/:id' => 'survey#view', :format => false
+   post 'survey/submit_process/:id' => 'survey#submit_process', :format => false
+
+   get 'survey/complete' => 'survey#complete', :format => false
+
+
+
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-   match ':controller(/:action(/:id))(.:format)'
 end
